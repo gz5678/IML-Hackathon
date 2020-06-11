0a61d2e582fa027d551bc0c656a6fd2e681d9621
Gil Zivor <gz5678@cs.huji.ac.il>
2020-06-11 23:15:56 +0300

    Adding models and running decsion tree with boost

---
+++ task1/src/CV_func.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from plotnine import *
+#import preprocessing_funcs as pre_funcs
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV
+
+# importing Various regression algorithms
+#from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import Lasso,LinearRegression,Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,BaggingRegressor
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+
+
+def my_cross(X_train, y_train, model):
+ """""
+ :param X_train: Feature matrix
+ :param y_train: Response vector 
+ :param model: Model to use, e.g. model = linear_model.Lasso()
+ add tuning parameter if needed
+ :return 5 fold cross validation test score 
+ """""""""
+ cv = cross_validate(model, X_train, y_train, scoring='neg_mean_squared_error')
+ return cv['test_score']
+
+
+def best_tune(X_train, y_train, model, params):
+ """""
+ :param X_train: Feature matrix
+ :param y_train: Response vector 
+ :param model: Model to use, e.g. model = linear_model.Lasso()
+ :param params: 
+ :return best tuning parameter of 5 fold cross validation 
+ """""""""
+ cv = GridSearchCV(model, params, cv=5).fit(X_train, y_train)
+ return cv
+
+
+# Example
+# best_tune(trainX, trainY, KNeighborsClassifier(), param_grid)
+
+# prep model_list
+Las = Lasso()
+LinR = LinearRegression()
+Rid = Ridge()
+Rfc = RandomForestRegressor(random_state=2)
+Dtc = DecisionTreeRegressor(random_state=2)
+Boost_Lin = AdaBoostRegressor(base_estimator=LinR, random_state=2)
+Boost_las = AdaBoostRegressor(base_estimator=Las, random_state=2)
+Boost_rid = AdaBoostRegressor(base_estimator=Rid, random_state=2)
+Bg_Lin = BaggingRegressor(base_estimator=LinR, random_state=2)
+Bg_las = BaggingRegressor(base_estimator=Las, random_state=2)
+Bg_rid = BaggingRegressor(base_estimator=Rid, random_state=2)
+Boost_dtc = AdaBoostRegressor(base_estimator=Dtc, random_state=2)
+# Las, LinR, Rid, Dtc, Rfc, Boost_Lin, Boost_las, Boost_rid, Bg_Lin, Bg_las, Bg_rid
+# 'Lasso', 'Linear Regression', 'Ridge', 'Random forest Regressor', 'Decision Tree Regressor',
+#               'Boosted Linear',
+#               'Boosted Lasso', 'Boosted Ridge', 'Bagged Linear', 'Bagged Lasso', 'Bagged Ridge'
+
+
+zipped = zip([Boost_dtc],
+             ['Boosted Decision Tree'])
+
+
+def run_model(trainX, trainY, model_list=zipped):
+ """""
+ :param X_train: Feature matrix
+ :param y_train: Response vector 
+ :param model_list: 2 zipped lists, one with model objects, one with model name. 
+ e.g. zip([Las],['Lasso'])
+ :return Table of errors (estimated by cross validation) of each model
+ """""""""
+
+ # Tune parameters
+ model_params = {'Random forest Regressor': {'max_depth': [80, 90, 100, 110], 'min_samples_split': [8, 10, 12],
+                                             'n_estimators': [100, 200]},
+                 'Lasso': {'alpha': range(1, 10)},
+                 'Ridge': {'alpha': range(1, 10)},
+                 'Decision Tree Regressor': {'max_depth': [80, 90, 100, 110], 'min_samples_split': [8, 10, 12]}}
+
+ #####
+ scores = []
+ scores_boost = []
+ scores_bag = []
+ for model, name in model_list:
+  boost = AdaBoostRegressor(base_estimator=model)
+  bag = BaggingRegressor(base_estimator=model)
+
+  if name in model_params.keys():
+
+   scores_boost.append(best_tune(trainX, trainY, boost, model_params[name]))
+   scores_bag.append(best_tune(trainX, trainY, bag, model_params[name]))
+
+   scores.append(best_tune(trainX, trainY, model, model_params[name]))
+  else:
+   scores_boost.append(my_cross(trainX, trainY, boost))
+   scores_bag.append(my_cross(trainX, trainY, bag))
+
+   scores.append(my_cross(trainX, trainY, model))
+
+ return scores_boost, scores_bag, scores
--- task1/src/model.py
+++ task1/src/model.py
@@ -17,7 +17,7 @@ import feather
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import preprocessing_funcs as pre_funcs
-from sklearn.linear_model import LinearRegression
+import CV_func as models_exec
 
 class FlightPredictor:
     def __init__(self, path_to_weather='all_weather_data.csv'):
@@ -26,47 +26,51 @@ class FlightPredictor:
         @param path_to_weather: The path to a csv file containing weather data.
         """
         np.random.seed(0)
-        train_data = pd.read_csv('train_data.csv')
-        weather_data = pd.read_csv(path_to_weather).rename(columns={'day': 'FlightDate'})
-        # Fix date format
-        train_data['FlightDate'] = train_data['FlightDate'].apply(lambda x: re.sub(r'(\d\d)(\d\d)(-\d+-)(\d+)',
-                                                                                   r'\4\3\2',
-                                                                                   x))
-        merged_table = pre_funcs.merge_tables(train_data, weather_data)
+        np.set_printoptions(precision=5)
+        # train_data = pd.read_csv('train_data.csv')
+        # weather_data = pd.read_csv(path_to_weather).rename(columns={'day': 'FlightDate'})
+        # # Fix date format
+        # train_data['FlightDate'] = train_data['FlightDate'].apply(lambda x: re.sub(r'(\d\d)(\d\d)(-\d+-)(\d+)',
+        #                                                                            r'\4\3\2',
+        #                                                                            x))
+        # merged_table = pre_funcs.merge_tables(train_data, weather_data)
         # merged_table.to_pickle('merged_table.pkl')
-        # merged_table = pd.read_pickle('merged_table.pkl')
-        y = merged_table['ArrDelay']
-        X = merged_table.drop(columns=['ArrDelay', 'DelayFactor', 'Tail_Number', 'Flight_Number_Reporting_Airline'])
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
-        X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, shuffle=True)
-        X_train = pre_funcs.fix_weather_data(X_train)
-        X_train = pre_funcs.all_categoricals(X_train)
-        
-        X_val= pre_funcs.fix_weather_test_data(X_val)
-        X_val= pre_funcs.all_categoricals(X_val)
-        
-        X_test = pre_funcs.fix_weather_test_data(X_test)
-        X_test = pre_funcs.all_categoricals(X_test)
-        
-        ## remove na's
-        y_train = y_train[-X_train.isnull().any(axis=1)]
-        X_train = X_train[-X_train.isnull().any(axis=1)]
-
-        X_train = pre_funcs.date_to_timestamp(X_train)
+        # # merged_table = pd.read_pickle('merged_table.pkl')
+        # y = merged_table['ArrDelay']
+        # X = merged_table.drop(columns=['ArrDelay', 'DelayFactor', 'Tail_Number', 'Flight_Number_Reporting_Airline'])
+        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+        # X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, shuffle=True)
+        # X_train = pre_funcs.fix_weather_data(X_train)
+        # X_train = pre_funcs.all_categoricals(X_train)
+        #
+        # X_val= pre_funcs.fix_weather_test_data(X_val)
+        # X_val= pre_funcs.all_categoricals(X_val)
+        #
+        # X_test = pre_funcs.fix_weather_test_data(X_test)
+        # X_test = pre_funcs.all_categoricals(X_test)
+        #
+        # ## remove na's
+        # y_train = y_train[-X_train.isnull().any(axis=1)]
+        # X_train = X_train[-X_train.isnull().any(axis=1)]
+        #
+        # X_train = pre_funcs.date_to_timestamp(X_train)
         # X_train.to_pickle('x_train.pkl')
         # y_train.to_pickle('y_train.pkl')
-        # X_train = pd.read_pickle('x_train.pkl')
-        # y_train = pd.read_pickle('y_train.pkl')
+        X_train = pd.read_pickle('x_train.pkl')
+        y_train = pd.read_pickle('y_train.pkl')
         X_train_short = X_train.iloc[:50000, :]
         y_train_short = y_train.iloc[:50000]
         full_df = pd.concat([X_train_short, y_train_short], axis=1)
         full_df = full_df.dropna()
         y_train_short = full_df['ArrDelay']
         X_train_short = full_df.drop(columns=['ArrDelay'])
-        model = LinearRegression().fit(X_train_short, y_train_short)
-        y_pred = model.predict(X_train_short)
-        print(mean_squared_error(y_train_short, y_pred))
-        print(mean_squared_error(y_train_short, np.full(y_train_short.shape, y_train_short.mean())))
+        scores_boost, scores_bag, scores = models_exec.run_model(X_train_short, y_train_short)
+        print("The boost scores is: ")
+        print(scores_boost)
+        print("The bag scores is: ")
+        print(scores_bag)
+        print("The scores is: ")
+        print(scores)
         raise NotImplementedError
 
     def predict(self, x):
